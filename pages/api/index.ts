5ba7b9d851a019842e4d6f0d73d11f0003d5ffa6
Marcos <marcossandim21@gmail.com>
2023-06-28 20:57:22 -0300

    modulo 01 concluido

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(
-    request: NextApiRequest,
-    response: NextApiResponse
+  request: NextApiRequest,
+  response: NextApiResponse
 ) {
-    console.log(request.headers);
-    response.status(200).json({ message: "Olá mundo!" });
+  console.log(request.headers);
+  response.status(200).json({ message: "Olá mundo!" });
 }
